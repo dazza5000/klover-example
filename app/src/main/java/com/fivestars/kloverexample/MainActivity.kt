b01252f4bce4bd3956b9fe2c9182c43458d10d36
“darran-kelinske-fivestars” <darran.kelinske@fivestars.com>
2020-10-21 17:13:33 -0500

    [] Add buttons to send Clover intents
               high-level explanation of what the changes do.
    
    [](https://fivestars.atlassian.net/browse/)

--- app/src/main/java/com/fivestars/kloverexample/MainActivity.kt
+++ app/src/main/java/com/fivestars/kloverexample/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.fivestars.kloverexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import com.clover.sdk.util.CloverAccount
+import com.clover.sdk.v1.Intents
 import com.clover.sdk.v3.order.OrderConnector
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -16,6 +19,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.tender_button)?.setOnClickListener {
+            Intent(Intents.ACTION_MERCHANT_TENDER).run {
+                startActivity(this)
+            }
+        }
+
+        findViewById<Button>(R.id.pay_button)?.setOnClickListener {
+            Intent(Intents.ACTION_CLOVER_PAY).run {
+                startActivity(this)
+            }
+        }
 
         GlobalScope.launch(Dispatchers.IO) {
            orderConnector = OrderConnector(this@MainActivity, CloverAccount.getAccount(this@MainActivity), null)
