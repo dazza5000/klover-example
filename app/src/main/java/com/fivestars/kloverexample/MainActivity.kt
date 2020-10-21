890dd841bf8be2b7721f8bbe8f690f90976c0b96
Darran Kelinske <darran.kelinske@gmail.com>
2020-10-20 21:49:35 -0500

    Add simple OnOrderChangedListener

--- app/src/main/java/com/fivestars/kloverexample/MainActivity.kt
+++ app/src/main/java/com/fivestars/kloverexample/MainActivity.kt
@@ -2,10 +2,25 @@ package com.fivestars.kloverexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.clover.sdk.util.CloverAccount
+import com.clover.sdk.v3.order.OrderConnector
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private var orderConnector: OrderConnector? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch(Dispatchers.IO) {
+           orderConnector = OrderConnector(this@MainActivity, CloverAccount.getAccount(this@MainActivity), null)
+        }
+
+        orderConnector?.addOnOrderChangedListener { orderId, selfChange -> Log.d("darran", "$orderId $selfChange") }
     }
 }
